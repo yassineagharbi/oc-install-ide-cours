a2f130e3abe183367c189466bfa0040f62e4353f
yassine.agharbi <yassine.agharbi@FR-L5480535.groupinfra.com>
2022-11-07 22:27:32 +0100

    commit yassine

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,8 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int j;
+		int j; 
+		
 		for(j=0; j<3; j++) {
 			System.out.println("Hello bugs yassine !");
 		}
