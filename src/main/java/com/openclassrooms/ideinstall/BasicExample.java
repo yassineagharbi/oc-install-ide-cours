744c71731c893627af63be2e9263148a4fbe027c
yassine.agharbi <yassine.agharbi@FR-L5480535.groupinfra.com>
2022-11-07 22:22:07 +0100

    new version YA

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int j;
 		for(j=0; j<3; j++) {
-			System.out.println("Hello bugs !");
+			System.out.println("Hello bugs yassine !");
 		}
 	}
 }
